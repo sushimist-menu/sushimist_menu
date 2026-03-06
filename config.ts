8e44810943ff079f964ba453a1fe15211a254c0b
Diego <diegoaep07@proton.me>
2026-03-06 05:36:57 -0500

    Testing Auto Update

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const SITE_CONFIG = {
   name: 'SUSHI MIST',
-  description: '“Más que sushi, una experiencia.”',
+  description: '“Más que sushi, una experiencia...”',
   whatsappNumber: '1234567890', // Format: Country code + number without + or 00
   currency: '$',
   deliveryArea: 'Envíos a domicilio en toda la ciudad.',
